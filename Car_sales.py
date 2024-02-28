fdce062796d0efb6b7d279797f923924ff21f671
Zai13Zai13 <161561162+Zai13Zai13@users.noreply.github.com>
2024-02-29 00:24:27 +0800

    Add files via upload

---
+++ Car_sales.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("Car Sales")
+st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
